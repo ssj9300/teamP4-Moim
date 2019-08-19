9d5b3eaf20acd3666a9e56cc187e300047d7212d
ssj9300 <52514226+ssj9300@users.noreply.github.com>
2019-08-19 11:54:05 +0900

    로그인 로그아웃까지 완련된 깃

---
+++ MainActivity.java
@@ -0,0 +1,233 @@
+package com.example.moimproject4t;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.kakao.auth.ApiResponseCallback;
+import com.kakao.auth.AuthService;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.auth.network.response.AccessTokenInfoResponse;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.LoginButton;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.usermgmt.response.model.User;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.util.helper.log.Logger;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private SessionCallback callback;
+
+    Button button;
+    Button button_logout;
+    LoginButton btn_kakao;
+
+    String token;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.button_login);
+        btn_kakao=findViewById(R.id.btn_kakao_login);
+        button_logout=findViewById(R.id.button_logout);
+
+        button.setOnClickListener(this);
+        button_logout.setOnClickListener(this);
+
+        getHash();
+        permissionCheck();
+
+        //세션콜
+        callback = new SessionCallback();
+        Session.getCurrentSession().addCallback(callback);
+    }
+
+    //해쉬키얻기 + 퍼미션 체크 영역
+    private void permissionCheck() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+            }
+        }
+    }
+
+    private void getHash() {
+
+        try{                                                                    //manifest에서 확인 가능한 패키지 네임
+            PackageInfo info = getPackageManager().getPackageInfo("com.example.moimproject4t", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures){
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("[hash]", "hash="+ Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_login:
+                btn_kakao.performClick();
+                break;
+
+            case R.id.button_logout:
+                logout();
+                break;
+        }
+
+    }
+
+
+    //온클릭시 동작할 메소드
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)){
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    //세션상태 변경시 동작
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Session.getCurrentSession().removeCallback(callback);
+    }
+
+    private class SessionCallback implements ISessionCallback{
+        //세션 오픈 성공
+        @Override
+        public void onSessionOpened() {
+            //카카오톡에서 정보를 받아올 메소드
+            requestMe();
+        }
+
+        //세션 오픈 실패
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            if (exception!=null){
+                Logger.e(exception);
+            }
+
+        }
+    }
+
+    private void requestMe() {
+        List<String> keys = new ArrayList<>();
+
+        UserManagement.getInstance().me(keys, new MeV2ResponseCallback() {
+            //v1에서는 세션종료에러, 미가입자에러, 그외 에러가 있었으나 기존 v1이 종료되고 v2가 적용됨에 따라 비회원 에러는 사라짐
+
+            //의도치 않은 세션 종료로 인한 에러
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+                Toast.makeText(MainActivity.this, "세션 종료", Toast.LENGTH_SHORT);
+                Log.e("[error]","세션 종료로 인한 에러 발생");
+
+            }
+
+            //세션 종료로 인한 에러를 제외한 모든 에러
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                String error_message ="SessionClosed외의 에러 발생"+errorResult;
+                Logger.d(error_message);
+                Log.d("[error]", error_message);
+            }
+
+            @Override
+            public void onSuccess(MeV2Response response) {
+                Long id = response.getId();
+                token=Long.toString(id);
+                String name=response.getNickname();
+
+                String thumb = response.getThumbnailImagePath();
+                String profile = response.getProfileImagePath();
+
+                Log.d("[profile]", "token:"+token);
+                Log.d("[profile]", "name:"+name);
+                Log.d("[profile]", "thumb:"+thumb);
+                Log.d("[profile]", "profile:"+profile);
+
+                requestAccessTokenInfo();
+            }
+        });
+    }
+
+    private void requestAccessTokenInfo() {
+        AuthService.getInstance().requestAccessTokenInfo(new ApiResponseCallback<AccessTokenInfoResponse>() {
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+                Toast.makeText(MainActivity.this, "세션 종료", Toast.LENGTH_SHORT);
+                Log.e("[error]","세션 종료로 인한 에러 발생");
+            }
+
+
+
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                Logger.e("failed to get access token info. msg=" + errorResult);
+            }
+
+            @Override
+            public void onSuccess(AccessTokenInfoResponse accessTokenInfoResponse) {
+                long userId = accessTokenInfoResponse.getUserId();
+                String token2=Long.toString(userId);
+                Logger.d("this access token is for userId=" + userId);
+                Log.d("[profile]", "토큰2 : " + token2);
+
+                long expiresInMilis = accessTokenInfoResponse.getExpiresInMillis();
+                String exTime = Long.toString(expiresInMilis);
+                Logger.d("this access token expires after " + expiresInMilis + " milliseconds.");
+                Log.d("[profile]", "토큰유효시간 :"+ exTime +"m/s");
+            }
+        });
+    }
+
+    //로그아웃
+    private void logout() {
+        UserManagement.getInstance().requestLogout(new LogoutResponseCallback() {
+            @Override
+            public void onCompleteLogout() {
+                Toast.makeText(MainActivity.this,"로그아웃 되었습니다.",Toast.LENGTH_SHORT);
+            }
+        });
+    }
+
+}
